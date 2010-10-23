5984b75a1b893e3d141af8432450f29927fba5e6
chrhad081 <chrhad081@2bb16497-34e7-7611-d7d6-93f8a71f35b8>
2010-10-23 10:14:05 +0000

    CH: sender email tolong di-comment dan uncomment sesuai keperluan.

--- Avatar/Avatar-ejb/src/java/Support/EmailSender.java
+++ Avatar/Avatar-ejb/src/java/Support/EmailSender.java
@@ -19,7 +19,8 @@ public class EmailSender implements EmailSenderLocal {
         //Atribut
         //SmtpServer : tergantung dari apa yang dipakai, jika di kampus maka pakai smtp.gmail.com.
 	//Jika di rumah/kosan pakai speedy, smtp yang digunakan  adalah smtp.telkom.net
-	private static final String smtpServer = "smtp.gmail.com";
+	//private static final String smtpServer = "smtp.gmail.com";
+        private static final String smtpServer = "smtp.melsa.net.id";
 
         public static void sendEmail(String to, String from , String cc, String subject, String body) throws Exception
         {
