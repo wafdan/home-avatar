ef263568bfab997ac668039644acd24bc35147b9
mail.to.mukti@gmail.com <mail.to.mukti@gmail.com@2bb16497-34e7-7611-d7d6-93f8a71f35b8>
2010-11-16 04:06:16 +0000

    [Eka] halaman mengelola reservasi u/ notif + send email

--- Avatar/Avatar-ejb/src/java/KelolaReservasi/MengelolaReservasiController.java
+++ Avatar/Avatar-ejb/src/java/KelolaReservasi/MengelolaReservasiController.java
@@ -18,6 +18,7 @@ import AvatarEntity.RoomReservationJpaController;
 import AvatarEntity.exceptions.IllegalOrphanException;
 import AvatarEntity.exceptions.NonexistentEntityException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
@@ -76,20 +77,53 @@ public class MengelolaReservasiController {
         return due;
     }*/
 
+    public DueReservation getDueReservation(Integer reservationId) {
+        Reservation cur = rc.findReservation(reservationId);
+        Iterator<ReservationItem> rii = cur.getReservationItemCollection().iterator();
+        int index = 0;
+        Date current = new Date();
+        Date earliestDate = null;
+        ReservationItem dueResvItem = null;
+        while (rii.hasNext()) {
+            ReservationItem curRes = rii.next();
+            if (curRes instanceof RoomReservation) {
+                if (index == 0) {
+                    earliestDate = ((RoomReservation) curRes).getEntryDate();
+                    dueResvItem = curRes;
+                } else if (earliestDate.after(((RoomReservation) curRes).getEntryDate())) {
+                    earliestDate = ((RoomReservation) curRes).getEntryDate();
+                    dueResvItem = curRes;
+                }
+            } else if (curRes instanceof HallReservation) {
+                if (index == 0) {
+                    earliestDate = ((HallReservation) curRes).getUseDate();
+                    dueResvItem = curRes;
+                } else if (earliestDate.after(((HallReservation) curRes).getUseDate())) {
+                    earliestDate = ((HallReservation) curRes).getUseDate();
+                    dueResvItem = curRes;
+                }
+            }
+            index++;
+        }
+        DueReservation res = null;
+        if (cur != null) {
+            if (earliestDate.getTime() - current.getTime() <= 3*Timer.ONE_DAY) {
+                res = new DueReservation(cur,dueResvItem);
+            }
+        }
+        return res;
+    }
+
     public void deleteReservation (Integer reservationId) throws IllegalOrphanException, NonexistentEntityException {
         Reservation res = rc.findReservation(reservationId);
-        Iterator<ReservationItem> i = res.getReservationItemCollection().iterator();
-        while (i.hasNext()) {
-            ReservationItem curRes = i.next();
-            /*if (curRes instanceof RoomReservation) {
-                roc.destroy(curRes.getReservationItemId());
-            } else if (curRes instanceof HallReservation) {
-                hc.destroy(curRes.getReservationItemId());
-            } else if (curRes instanceof OtherServicesReservation) {
-                oc.destroy(curRes.getReservationItemId());
-            }*/
-            ric.destroy(curRes.getReservationItemId());
+        Object[] ris = res.getReservationItemCollection().toArray();
+        for (int i=0; i<ris.length; i++) {
+            ric.destroy(((ReservationItem) ris[i]).getReservationItemId());
         }
         rc.destroy(reservationId);
+    }
+
+    public Collection<ReservationItem> getReservationItemById(Integer id) {
+        return rc.findReservation(id).getReservationItemCollection();
     }
 }
